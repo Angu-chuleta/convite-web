fade8286dfa10e802dab4afe66c30bb71311d565
David Vilaça <davidpvilaca@outlook.com>
2017-04-15 13:01:20 -0300

    Criado storage service

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+import { HttpModule } from '@angular/http'
+import { RouterModule } from '@angular/router'
+import { StorageService } from './storage'
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpModule,
+    RouterModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [
+    StorageService
+  ]
 })
 export class CoreModule { }
---
+++ src/app/core/storage/storage.service.ts
@@ -0,0 +1,100 @@
+import { Injectable } from '@angular/core'
+
+@Injectable()
+export class StorageService {
+
+  /**
+   *
+   *
+   * @param {string} key
+   * @param {*} value
+   *
+   * @memberOf StorageService
+   */
+  public setItem(key: string, value: any) {
+    localStorage.setItem(key, this.convertValue(value))
+  }
+
+  /**
+   *
+   *
+   * @param {any} k
+   * @returns
+   *
+   * @memberOf Store
+   */
+  public getItem(key: string | number) {
+    let value = typeof key !== 'number' ? localStorage.getItem(key) : localStorage.getItem(localStorage.key(key))
+    return this.unconvertValue(value)
+  }
+
+  /**
+   *
+   *
+   * @param {any} k
+   *
+   * @memberOf Store
+   */
+  public removeItem(key: string) {
+    localStorage.removeItem(key)
+  }
+
+  /**
+   *
+   *
+   * @param {string} key
+   * @param {*} updates
+   *
+   * @memberOf Storage
+   */
+  public updateItem(key: string, updates: any) {
+    const obj = this.getItem(key)
+    if (typeof obj === 'string' || typeof obj === 'boolean') {
+      this.setItem(key, updates)
+      return
+    }
+    this.setItem(key, Object.assign(obj || {}, updates))
+  }
+
+  /**
+   *
+   *
+   *
+   * @memberOf Store
+   */
+  public clear() {
+    localStorage.clear()
+  }
+
+  /**
+   *
+   *
+   * @private
+   * @param {any} value
+   * @returns
+   *
+   * @memberOf Store
+   */
+  private convertValue(value: any): any {
+    return typeof value !== 'object' ? value : JSON.stringify(value)
+  }
+
+  /**
+   *
+   *
+   * @private
+   * @param {any} value
+   * @returns
+   *
+   * @memberOf Store
+   */
+  private unconvertValue(value: string) {
+    if (value !== null) {
+      if (value.indexOf('{') === 0 || value.indexOf('[') === 0) {
+        return JSON.parse(value)
+      } else {
+        return value
+      }
+    }
+  }
+}
