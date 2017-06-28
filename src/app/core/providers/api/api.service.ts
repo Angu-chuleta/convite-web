5de534536b70d50d547df3cf2e13d48b5ae41d04
David Vilaça <davidpvilaca@outlook.com>
2017-06-28 12:46:16 -0300

    criado serviço básico de api e implementado para eventos

--- src/app/core/providers/api/api.service.ts
+++ src/app/core/providers/api/api.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@angular/core'
+import { EventsApiService } from './events-api.service'
 
 @Injectable()
-export class ApiService { }
+export class ApiService {
+
+  /**
+   * API de eventos
+   *
+   * @type {EventsApiService}@memberof ApiService
+   */
+  public events: EventsApiService
+
+  /**
+   * Creates an instance of ApiService.
+   * @param {EventsApiService} events
+   * @memberof ApiService
+   */
+  constructor (events: EventsApiService) {
+    this.events = events
+  }
+
+}
